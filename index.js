5c9063b2d9fdcf413f917b1fd5388c5d495b161b
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-21 12:19:09 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config;
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const usersRouter = require('./users/router');
+const postsRouter = require('./posts/router');
+
+const server = express();
+const port = process.env.PORT || 4000;
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+server.use('/api/users', usersRouter);
+server.use('/', (req, res) => res.send('API up and running!'));
+
+server.listen(port, () =>
+  console.log(`\n*** API running on  port ${port} ***\n`)
+);
